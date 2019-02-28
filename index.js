3141752218541789866341096b847a26530a06a6
Marius Lichtblau <marius@lichtblau.io>
2019-02-28 19:34:01 +0100

    First read

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const fs = require('fs');
 
-let fileContents = fs.readFileSync('yarn.lock', { encoding: 'utf8' });
-
+let fileContents = fs.readFileSync('c_memorable_moments.txt', { encoding: 'utf8' });
+let lines = fileContents.split('\n');
+let firstLine = lines.shift();
+let data = lines.map(line => {
+  let splitLine = line.split(' ');
+  return {
+    orientation: splitLine[0],
+    tags: splitLine.slice(2)
+  };
+});
